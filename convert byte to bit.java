0f5f40a9776c755dcfcec707f89ff0fc3be99b3a
askarbatcha <askarbatcha160@gmail.com>
2025-12-12 12:12:45 +0530

    Create convert byte to bit.java

---
+++ convert byte to bit.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class BytesToBits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter bytes: ");
+        int bytes = sc.nextInt();
+
+        int bits = bytes * 8;
+
+        System.out.println("Bits = " + bits);
+    }
+}
