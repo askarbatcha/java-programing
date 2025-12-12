e3adf0712e7d6c3904e798b3899deefad17e764e
askarbatcha <askarbatcha160@gmail.com>
2025-12-12 11:57:06 +0530

    Create print the no in two lines .java

---
+++ print the no in two lines .java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class q1{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("10 \n20" );
+        // System.out.println("20"); 
+    }
+} 
