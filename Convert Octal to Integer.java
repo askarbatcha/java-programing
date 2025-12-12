1cd2a264ed0dd4a7bf0a15522aef85885d5240ea
askarbatcha <askarbatcha160@gmail.com>
2025-12-12 12:25:19 +0530

    Create Convert Octal to Integer.java

---
+++ Convert Octal to Integer.java
@@ -0,0 +1,7 @@
+public class Q10 {
+    public static void main(String[] args) {
+        String octal = "75";
+        int decimal = Integer.parseInt(octal, 8);
+        System.out.println(decimal);
+    }
+}
