b28939ac2aeaabe67d007067c6e4bea949c9f782
askarbatcha <askarbatcha160@gmail.com>
2025-12-12 12:17:19 +0530

    Create Cost Price (CP = SP − Loss).java

---
+++ Cost Price (CP = SP − Loss).java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class CostPrice {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Selling Price: ");
+        double sp = sc.nextDouble();
+
+        System.out.print("Enter Loss: ");
+        double loss = sc.nextDouble();
+
+        double cp = sp - loss;
+
+        System.out.println("Cost Price = " + cp);
+    }
+}
