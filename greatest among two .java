d7a9c2181980ace80bbd49158157ef5e24520f5f
askarbatcha <askarbatcha160@gmail.com>
2025-12-13 16:01:40 +0530

    Create greatest among two .java

---
+++ greatest among two .java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class c3{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the values");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        // int c = sc.nextInt();
+        // System.out.print 
+        {
+        if (a > b) {
+            System.out.println("Greatest number is: "+ a);
+        } else if (b > a) {
+            System.out.println("Greatest number is: "+ b);
+        } else {
+            System.out.println("both the numbers are equal");
+
+        }
+    }
+
+    }
+}
